c246841ef6a3965187c48772c9ac455876fbae4d
Carl-Mikael Bergbom <89707903+carl-mikael@users.noreply.github.com>
2026-04-30 14:32:36 +0200

    fix food

--- Source/Snake3D/Public/Food.h
+++ Source/Snake3D/Public/Food.h
@@ -22,14 +22,4 @@ protected:
 public:	
 	// Sets default values for this actor's properties
 	AFood();
-	
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
 };
